188eb6e02348e374195449977212756a0db3bad6
tour21 <tour20@163.com>
2018-11-25 22:50:02 +0800

    first program

---
+++ src/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: '主页'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+
+</style>
+
